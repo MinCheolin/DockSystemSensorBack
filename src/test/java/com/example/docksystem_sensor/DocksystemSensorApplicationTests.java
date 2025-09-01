28647474a2087c97a3d53ca12f2dfab7ce1ff4ed
김민철 <rlaalscjf739789@gamil.com>
2025-09-01 09:35:53 +0900

    첫커밋

---
+++ src/test/java/com/example/docksystem_sensor/DocksystemSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docksystem_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocksystemSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
